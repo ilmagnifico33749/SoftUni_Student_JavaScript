e49a9f40dbeb823e3b97c26b8df12879009fe057
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-05-30 20:32:40 +0300

    Update and rename 04. Reverse in Place.js to 04. Reverse in Place - v1.js

---
+++ 02. Module_Fundamentals_May_2025/09. Lab - Arrays/04. Reverse in Place - v1.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+let arrRev = arr.reverse();
+	console.log(arrRev.join(" "));
+}
+
+solve(['a', 'b', 'c', 'd', 'e']);
+solve(['abc', 'def', 'hig', 'klm', 'nop']);
+solve(['33', '123', '0', 'dd']);
