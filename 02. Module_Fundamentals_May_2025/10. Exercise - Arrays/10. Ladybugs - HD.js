2d6b76920b944b62ecf695c7ff6af1c27c716e54
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-06-24 15:35:28 +0300

    Update 10. Ladybugs - HD.js

--- 02. Module_Fundamentals_May_2025/10. Exercise - Arrays/10. Ladybugs - HD.js
+++ 02. Module_Fundamentals_May_2025/10. Exercise - Arrays/10. Ladybugs - HD.js
@@ -1,26 +1,59 @@
-function solve(userInput) {
-    let fieldSizeColumns = userInput[0];
-    let fieldSizeRows = 1;
-    let initialPositionsLadybugs = userInput[1];
-    let currentPositionsLadybugs = initialPositionsLadybugs;
-    for (let indexFuturePosition = 2; indexFuturePosition < userInput.length; indexFuturePosition++) {
-        let finalNewPosition;
-        let nextPosition = userInput[indexFuturePosition];
-        console.log(nextPosition);
-        let elementsNextPosition = nextPosition.split(" ");
-        console.log(elementsNextPosition);
-        let currentPosition = elementsNextPosition[0];
-        let directionNewPostion = elementsNextPosition[1];
-        let newPositionColumn = elementsNextPosition[2];
+function ladybugs(arrInp) {
+  let fieldSize = Number(arrInp[0]);
+  let ladybugsPositionsArr = arrInp[1].split(" ").map(Number);
+  let commandsArr = [];
 
-        if (directionNewPostion === "left") {
-            
-        }
-        
+  for (let index = 2; index < arrInp.length; index++) {
+    commandsArr.push(arrInp[index]);
+  }
+
+  let field = new Array(fieldSize).fill(0);
+
+  for (let pos of ladybugsPositionsArr) {
+    if (pos >= 0 && pos < fieldSize) {
+      field[pos] = 1;
+    }
+  }
+
+  for (let element of commandsArr) {
+    let currentCommandArr = element.split(" ");
+    let initialPosition = Number(currentCommandArr[0]);
+    let direction = currentCommandArr[1];
+    let movementStep = Number(currentCommandArr[2]);
+
+    if (initialPosition < 0 || initialPosition >= fieldSize) continue;
+    if (field[initialPosition] !== 1) continue; 
+
+    field[initialPosition] = 0;
+
+    let nextPosition = initialPosition;
+
+    if (movementStep < 0) {
+      movementStep = Math.abs(movementStep);
+      direction = direction === 'right' ? 'left' : 'right';
     }
 
+    while (true) {
+      if (direction === 'right') {
+        nextPosition += movementStep;
+      } else {
+        nextPosition -= movementStep;
+      }
+
+      if (nextPosition < 0 || nextPosition >= fieldSize) {
+        break;
+      }
+
+      if (field[nextPosition] === 0) {
+        field[nextPosition] = 1;
+        break;
+      }
+    }
+  }
+
+  console.log(field.join(" "));
 }
 
-solve([ 3, '0 1', '0 right 1', '2 right 1' ]);
-// solve([ 3, '0 1 2', '0 right 1', '1 right 1', '2 right 1']);
-// solve([ 5, '3', '3 left 2', '1 left -2']);
+ladybugs([3, '0 1', '0 right 1', '2 right 1' ]);
+ladybugs([3, '0 1 2', '0 right 1', '1 right 1', '2 right 1']);
+ladybugs([5, '3', '3 left 2', '1 left -2']);
