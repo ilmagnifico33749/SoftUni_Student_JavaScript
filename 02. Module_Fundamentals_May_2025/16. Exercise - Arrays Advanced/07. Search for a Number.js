34a2218bdb9266a406be58a054e598c22e265169
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-06-30 18:20:07 +0300

    30/June/2025 - 3

---
+++ 02. Module_Fundamentals_May_2025/16. Exercise - Arrays Advanced/07. Search for a Number.js
@@ -0,0 +1,18 @@
+function searchNumber(arr1, arr2) {
+    let arrayElsToMan = arr1;
+    let numElsToTake = arr2.shift();
+    let numElsToDel = arr2.shift();
+    let numToSearchFor = arr2.pop();
+
+    let count = arrayElsToMan
+        .splice(0, numElsToTake)
+        .splice(numElsToDel)
+        .filter(x => x==numToSearchFor)
+        .length;
+
+
+    console.log(`Number ${numToSearchFor} occurs ${count} times.`);
+}
+
+searchNumber([5, 2, 3, 4, 1, 6], [5, 2, 3]);
+searchNumber([7, 1, 5, 8, 2, 7], [3, 1, 5]);
