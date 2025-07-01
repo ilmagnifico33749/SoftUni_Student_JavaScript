6e69e402b01118c5a4442bbe768af65375d388d1
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-07-01 22:22:47 +0300

    Update 01. Person Info.js

---
+++ 02. Module_Fundamentals_May_2025/20. Lab - Objects and Classes/01. Person Info.js
@@ -0,0 +1,11 @@
+function personInfo(firstname, lastname, age){
+  let person =  {
+    firstName: firstname,
+    lastName: lastname,
+    age: age,
+  }
+  return person;
+}
+
+personInfo("Peter","Pan","20");
+personInfo("George","Smith","18");
