4d4ffcf799ca0ddff69a05b0f2c03772837f273a
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-07-03 18:03:00 +0300

    Update 06. Songs.js

---
+++ 02. Module_Fundamentals_May_2025/20. Lab - Objects and Classes/06. Songs.js
@@ -0,0 +1,42 @@
+function songs(arrInp) {
+  class Song {
+    constructor(typeList, name, time) {
+      this.typeList = typeList;
+      this.name = name;
+      this.time = time;
+    };
+  };
+
+  let songsNum = arrInp.shift();
+  let playlistToPlay = arrInp.pop();
+  for (let currentSong of arrInp) {
+    let currentSongDetails = currentSong.split("_");
+    let currentSongPlaylist = currentSongDetails[0];
+    let currentSongName = currentSongDetails[1];
+    let currentSongTime = currentSongDetails[2];
+    let song = new Song(currentSongPlaylist, currentSongName, currentSongTime)
+    
+  }
+}
+
+songs(
+  [3,
+  'favourite_DownTown_3:14',
+  'favourite_Kiss_4:16',
+  'favourite_Smooth Criminal_4:01',
+  'favourite']
+);
+songs(
+  [4,
+  'favourite_DownTown_3:14',
+  'listenLater_Andalouse_3:24',
+  'favourite_In To The Night_3:58',
+  'favourite_Live It Up_3:48',
+  'listenLater']
+);
+songs(
+  [2,
+  'like_Replay_3:15',
+  'ban_Photoshop_3:48',
+  'all']
+);
