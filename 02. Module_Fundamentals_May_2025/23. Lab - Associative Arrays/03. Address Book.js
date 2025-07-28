607d7a0af5baf0612826ae93f06400d58be2d322
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-07-29 01:43:45 +0300

    29/July/2025 - 1

---
+++ 02. Module_Fundamentals_May_2025/23. Lab - Associative Arrays/03. Address Book.js
@@ -0,0 +1,43 @@
+function addressBookfunction(arrInp) {
+    let addressBookMap = new Map();
+
+    for (let info of arrInp) {
+        let [name, address] = info.split(":");
+        addressBookMap.set(name, address);
+    }
+
+    let addressBookArray = Array
+        .from(addressBookMap.entries())
+        .sort((a, b) => a[0].localeCompare(b[0]));
+
+    for (let [name, address] of addressBookArray) {
+        console.log(`${name} -> ${address}`);
+    }
+
+}
+
+
+addressBookfunction(
+    [
+        'Tim:Doe Crossing',
+        'Bill:Nelson Place',
+        'Peter:Carlyle Ave',
+        'Bill:Ornery Rd'
+    ]
+);
+
+addressBookfunction(
+    [
+        'Bob:Huxley Rd',
+        'John:Milwaukee Crossing',
+        'Peter:Fordem Ave',
+        'Bob:Redwing Ave',
+        'George:Mesta Crossing',
+        'Ted:Gateway Way',
+        'Bill:Gateway Way',
+        'John:Grover Rd',
+        'Peter:Huxley Rd',
+        'Jeff:Gateway Way',
+        'Jeff:Huxley Rd'
+    ]
+);
