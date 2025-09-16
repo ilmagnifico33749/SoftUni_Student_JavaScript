b9683be10541da1a10da8240bee0565a67e414a4
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-09-16 22:14:49 +0300

    16/Sept/2025 - 1

---
+++ 03. Module_Advanced_September_2025/01. Lab - Syntax, Functions and Statements/01. Echo Function.js
@@ -0,0 +1,10 @@
+function echoFunction(strInp) {
+    let outpStr = strInp;
+    let outpStrLen = strInp.length;
+    console.log(outpStrLen);
+    console.log(outpStr);
+}
+
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
+
