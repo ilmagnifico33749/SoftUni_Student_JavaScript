3896616db11d5456b86c946300b1fe40d5b5e982
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-10-14 02:23:41 +0300

    14/Oct/2025 - 2

---
+++ 03. Module_Advanced_September_2025/04. Execise - Arrays and Nested Arrays/02. Print every N-th Element from an Array.js
@@ -0,0 +1,23 @@
+function printEveryNElement(arrInp, evElToPrint) {
+    let finalArr = [];
+    for (let i=0; i<arrInp.length; i+evElToPrint) {
+        finalArr.push(arrInp[i]);
+    }
+
+    return finalArr;
+}
+
+printEveryNElement(
+    ['5', '20', '31', '4', '20'],
+    2
+)
+
+printEveryNElement (
+    ['dsa', 'asd', 'test', 'tset'], 
+    2
+);
+
+printEveryNElement(
+    ['1', '2', '3', '4', '5'], 
+    6
+)
