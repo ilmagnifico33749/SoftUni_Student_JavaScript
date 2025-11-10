8e05e11e836aae938c4778267d1c7cc773ba3d53
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2025-11-10 19:02:43 +0200

    10/Nov/2025 - 3

--- 03. Module_Advanced_September_2025/06. Exercise - Objects and Composition/07. Towns to JSON.js
+++ 03. Module_Advanced_September_2025/06. Exercise - Objects and Composition/07. Towns to JSON.js
@@ -1,7 +1,38 @@
 function townToJSON(arrInp) {
-    for (let cityInfo of arrInp) {
-        console.log(cityInfo);
+    let result = []
+
+    let columnsNamesRaw = arrInp.shift()
+        .split("|")
+        .filter(part => part !== '')
+        .map(x => x.trim());
+
+    let columnNamesRefined = {};
+
+    for (let index=0; index<columnsNamesRaw.length; index++) {
+        let currentColumnName = columnsNamesRaw[index];
+        console.log(currentColumnName)
+        columnNamesRefined[`Column-${index}`] = currentColumnName;
     }
+
+    for (let cityInfo of arrInp) {
+        let cityInfoRefined = [];
+        let currentCityObject = {}
+        let [cityName, cityLatitude, cityLongitude] = cityInfo
+            .split("|")
+            .filter(part => part !== '')
+            .map(x => x.trim());
+        console.log(cityName, cityLatitude, cityLongitude); 
+        cityInfoRefined[cityName, cityLatitude, cityLongitude];
+        
+        for (let index in Object.keys(columnNamesRefined)) {
+            let currentColumnName = columnNamesRefined[index];
+            let currentCityStat = cityInfoRefined[index];
+            currentCityObject[currentColumnName] = currentCityStat;
+        }
+        console.log(currentCityObject)
+        result.push(currentCityObject);
+    }
+    console.log(JSON.stringify(result));
 }
 
 townToJSON(
@@ -12,10 +43,10 @@ townToJSON(
     ]
 )
 
-townToJSON(
-    [
-        '| Town | Latitude | Longitude |', 
-        '| Veliko Turnovo | 43.0757 | 25.6172 |', 
-        '| Monatevideo | 34.50 | 56.11 |'
-    ]
-);
+// townToJSON(
+//     [
+//         '| Town | Latitude | Longitude |', 
+//         '| Veliko Turnovo | 43.0757 | 25.6172 |', 
+//         '| Monatevideo | 34.50 | 56.11 |'
+//     ]
+// );
