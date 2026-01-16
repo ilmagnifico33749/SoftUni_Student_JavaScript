de345d9d229cbdb013bc9c8bfc3586886adb9441
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-01-16 20:42:57 +0200

    16/Jan/2026 - 1

--- 03. Module_Advanced_January_2026/02. EX - Syntax, Functions and Statements/07. Cooking by Numbers.js
+++ 03. Module_Advanced_January_2026/02. EX - Syntax, Functions and Statements/07. Cooking by Numbers.js
@@ -1,9 +1,26 @@
-function cookingByNumbers(arrInp) {
-    console.log(arrInp);
-    [number, ...arrOperations] = arrInp;
-    console.log(number);
-    console.log(arrOperations);
+function cookingByNumbers(num, ...arrOperations) {
+    number = Number(num);
+    for (let op of arrOperations) {
+        switch (op) {
+            case 'chop':
+                number /= 2;
+                break;
+            case 'dice':
+                number = Math.sqrt(number);
+                break;
+            case 'spice':
+                number += 1;
+                break;
+            case 'bake':
+                number *= 3;
+                break;
+            case 'fillet':
+                number *= 0.8;
+                break;
+        }
+        console.log(number);
+    }
 }
 
 cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
-// cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
