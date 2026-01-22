c8fe5e0e3e5f6b9294c304924868c08d61fd50dc
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-01-23 01:01:44 +0200

    23/Jan/2026 - 1

--- 03. Module_Advanced_January_2026/03. LAB - Arrays and Nested Arrays/11. Equal Neighbors.js
+++ 03. Module_Advanced_January_2026/03. LAB - Arrays and Nested Arrays/11. Equal Neighbors.js
@@ -1,7 +1,34 @@
 function equalNeighbors(matrixInp){
-   for (let arrayIndex in matrixInp) {
-    console.log(arrayIndex)
-   } 
+    let pairs = [];
+    let countPairs = 0;
+
+    for (let arrayIndex=0; arrayIndex<(matrixInp.length); arrayIndex++) {
+    let currentArray = matrixInp[arrayIndex];
+    
+        for (let elementIndex=0; elementIndex<(currentArray.length); elementIndex++) {
+            let currentElement = currentArray[elementIndex];
+
+            if (elementIndex<(currentArray.length-1)) {
+                let nextElementCurrentArray = currentArray[elementIndex+1];
+                if (currentElement == nextElementCurrentArray) {
+                    pairs.push([currentElement, nextElementCurrentArray]);
+                    countPairs++;
+                }
+            }
+            
+            if (arrayIndex<(matrixInp.length-1)) {
+                let nextArray = matrixInp[arrayIndex+1];
+                let nextElementNextArray = nextArray[elementIndex];
+                if (currentElement == nextElementNextArray) {
+                    pairs.push([currentElement, nextElementNextArray]);
+                    countPairs++;
+                }
+            }
+        }
+    }
+    // console.log(pairs.join(" | "));
+    console.log(countPairs);
+    // console.log(pairs.length);
 }
 
 equalNeighbors(
@@ -20,3 +47,11 @@ equalNeighbors(
         ['not', 'done', 'yet', '5']
     ]
 );
+
+equalNeighbors (
+    [
+        [2, 2, 5, 7, 4],
+        [4, 0, 5, 3, 4],
+        [2, 5, 5, 4, 2]
+    ]
+)
