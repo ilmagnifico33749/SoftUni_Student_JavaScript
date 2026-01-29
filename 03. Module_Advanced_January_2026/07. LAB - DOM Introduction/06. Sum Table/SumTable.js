989446b41cb804497afccd061b70d3323db10be9
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-01-29 23:46:14 +0200

    29/Jan/2026 - 5

--- 03. Module_Advanced_January_2026/07. LAB - DOM Introduction/06. Sum Table/SumTable.js
+++ 03. Module_Advanced_January_2026/07. LAB - DOM Introduction/06. Sum Table/SumTable.js
@@ -1,3 +1,24 @@
 function sumTable() {
+    // select first table
+    // select all rows
+    // filter first and last rows (headers and output)
+    // for each row
+    // -- select columns (children)
+    // -- take last column
+    // -- parse content as Number and add to sum
+    // print output in element with id "sum"
 
+    const rows = Array.from(document.querySelector('table').querySelectorAll('tr')).slice(1, -1);
+
+    let sum = 0;
+
+    for (let row of rows) {
+        const lastColumn = row.lastElementChild;
+        sum += Number(lastColumn.textContent);
+    }
+
+    console.log(sum)
+
+    document.getElementById('sum').textContent = sum.toFixed(2);
+    
 }
