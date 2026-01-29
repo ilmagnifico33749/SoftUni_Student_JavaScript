a4929f94783ed961312552bd8c4923223c5dd439
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-01-29 22:21:38 +0200

    29/Jan/2026 - 3

--- 03. Module_Advanced_January_2026/07. LAB - DOM Introduction/02. Collect List Items/CollectListItems.js
+++ 03. Module_Advanced_January_2026/07. LAB - DOM Introduction/02. Collect List Items/CollectListItems.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+    // select all list items
+    // extract and join text content
+    // print result in output filed
+
+    const items = Array.from(document.getElementsByTagName('li'));
+    const text = items.map(i => i.textContent);
+
+    const output = document.getElementById('result');
+    output.value = text.join('\n');
 }
