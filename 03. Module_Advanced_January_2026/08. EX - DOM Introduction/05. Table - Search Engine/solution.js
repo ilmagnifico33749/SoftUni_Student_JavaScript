3c0869518d77e7442324f6e1fdc7e9320ef29cb3
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-04 01:56:21 +0200

    04/Feb/2026 - 3

--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/05. Table - Search Engine/solution.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/05. Table - Search Engine/solution.js
@@ -31,7 +31,6 @@ function solve() {
                // TO SOMEHWO ADD CLASS SELECT
                currentRow.classList.add('select');
             }
-
          }
       }
 
--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/06. Format the Text/solution.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/06. Format the Text/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO
+  let textAreaUsrInp = document.getElementById('input');
+  let textUsrInp = textAreaUsrInp.value;
+  let arrTextUsrInp = textUsrInp.split(".");
+  let outputArea = document.getElementById('output');
+
+  let finalTextOutput = '';
+  let currentParagraph = [];
+
+  for (let index = 0; index < arrTextUsrInp.length; index++) {
+    let currentSentence = arrTextUsrInp[index].trim();
+    
+    if (currentSentence.length > -1) {
+
+
+      if (currentParagraph.length < 3) {
+        currentParagraph.push(`${currentSentence}.`);
+      }
+
+      
+      if (currentParagraph.length === 3 || index === (arrTextUsrInp.length-1)) {
+        finalTextOutput += `<p>${currentParagraph.join(" ")}</p>\n`
+        currentParagraph = [];
+      }
+    }
+  }
+
+  outputArea.innerHTML = finalTextOutput;
 }
