d72500fd2dcb8cd8ca0840c26f70f248a0edc501
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-03 21:35:55 +0200

    03/Feb/2026 - 3

--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/03. Accordion/accordion.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+ 
+    let button = document.getElementsByClassName("button")[0];
+    let buttonText = document.getElementsByClassName('button')[0];
+    let extraText = document.getElementById('extra').style
+    
+    if (extraText.display == 'none' || extraText.display == '') {
+        extraText.display = 'block';
+        buttonText.textContent = 'Less';
+    }
+
+    else if (extraText.display == 'block') {
+        extraText.display = 'none';
+        buttonText.textContent = 'More';
+    }
+
 }
