6126ea55666064e5abd80772efcef0376aadcc39
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-05 01:12:00 +0200

    05/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/06. Format the Text/solution.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/06. Format the Text/solution.js
@@ -1,27 +1,58 @@
+// function solve() {
+//   let textAreaUsrInp = document.getElementById('input');
+//   let textUsrInp = textAreaUsrInp.value;
+//   let arrTextUsrInp = textUsrInp.split(".");
+//   let outputArea = document.getElementById('output');
+
+//   let finalTextOutput = '';
+//   let currentParagraph = [];
+
+//   for (let index = 0; index < arrTextUsrInp.length; index++) {
+//     let currentSentence = arrTextUsrInp[index].trim();
+    
+//     if (currentSentence.length > 0) {
+
+
+//       if (currentParagraph.length < 3) {
+//         currentParagraph.push(`${currentSentence}.`);
+//       }
+
+      
+//       if (currentParagraph.length === 3 || index === (arrTextUsrInp.length-1)) {
+//         finalTextOutput += `<p>${currentParagraph.join(" ")}</p>\n`
+//         currentParagraph = [];
+//       }
+//     }
+//   }
+
+//   outputArea.innerHTML = finalTextOutput;
+// }
+//==========================================================================================
 function solve() {
   let textAreaUsrInp = document.getElementById('input');
   let textUsrInp = textAreaUsrInp.value;
-  let arrTextUsrInp = textUsrInp.split(".");
+
+  // Clean the array so the last real sentence is truly the last element
+  let arrTextUsrInp = textUsrInp
+      .split(".")
+      .map(s => s.trim())
+      .filter(s => s.length > 0);
+
   let outputArea = document.getElementById('output');
 
   let finalTextOutput = '';
   let currentParagraph = [];
 
   for (let index = 0; index < arrTextUsrInp.length; index++) {
-    let currentSentence = arrTextUsrInp[index].trim();
-    
-    if (currentSentence.length > -1) {
+    let currentSentence = arrTextUsrInp[index];
 
+    if (currentParagraph.length < 3) {
+      currentParagraph.push(currentSentence + ".");
+    }
 
-      if (currentParagraph.length < 3) {
-        currentParagraph.push(`${currentSentence}.`);
-      }
-
-      
-      if (currentParagraph.length === 3 || index === (arrTextUsrInp.length-1)) {
-        finalTextOutput += `<p>${currentParagraph.join(" ")}</p>\n`
-        currentParagraph = [];
-      }
+    if (currentParagraph.length === 3 || index === arrTextUsrInp.length - 1) {
+      finalTextOutput += `<p>${currentParagraph.join(" ")}</p>\n`;
+      currentParagraph = [];
     }
   }
 
--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/07. Hell's Kitchen/app.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/07. Hell's Kitchen/app.js
@@ -2,7 +2,41 @@ function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
    function onClick () {
-      //   TODO:
+      let userInput = document.querySelectorAll('textarea')[0].value;
       
+
+      let arrayDataFromUserInput = JSON.parse(userInput)
+      console.log(arrayDataFromUserInput);
+
+      let restaurantObj = new Object ({
+         name: "",
+         employees: [],
+         salaryAverage: undefined,
+         salaryMax: undefined
+      })
+
+      let employeeObj = {
+         name: "",
+         salary: undefined
+      }
+
+
+      let restaurantsData = [];
+
+      for (let index=0; index<arrayDataFromUserInput.length; index++) {
+         let currentElement = arrayDataFromUserInput[index];
+         console.log(currentElement);
+         let [restaurantNameString, restaurantEmployeesString] = currentElement.split(" - ");
+         console.log(restaurantNameString);
+         console.log(restaurantEmployeesString);
+         let currentRestaurantObject = restaurantObj
+         currentRestaurantObject.name = restaurantNameString;
+         for (let employeeInfo of restaurantEmployeesString.split(", ")) {
+            console.log(employeeInfo);
+         }
+      }
+
    }
 }
+
+["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]
