3791712bb5755d7d2a6cfd43eead2164f02f01cd
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-09 01:39:00 +0200

    09/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/08. Generate Report/report.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/08. Generate Report/report.js
@@ -1,3 +1,32 @@
 function generateReport() {
-    //TODO
+    let columnsCheckBoxes = document.querySelectorAll('thead input[type="checkbox"]');
+    let arrayColumnsCheckBoxes = Array.from(columnsCheckBoxes);
+    let outputMessageLocation = document.getElementById('output');
+    let rowsTable = document.querySelectorAll('tbody tr');
+    let arrayRowsTable = Array.from(rowsTable);
+    let checkedColumns = [];
+    let output = [];
+
+    for (let columnIndex in arrayColumnsCheckBoxes) {
+        let columnCurrent = columnsCheckBoxes[columnIndex];
+        let statusChecked = columnCurrent.checked;
+        if (statusChecked) {
+            checkedColumns.push([columnIndex, columnCurrent]);
+        }
+    }
+
+    for (let row of arrayRowsTable) {
+        let rowElements = row.querySelectorAll('td');
+        let newSelectionObject = {}
+        for (let columnInfo of checkedColumns) {
+            let columnTableIndex = columnInfo[0];
+            let selectedColumn = columnInfo[1];
+            let selectedColumnRowCellValue = rowElements[columnTableIndex].textContent;
+            newSelectionObject[selectedColumn.name] = selectedColumnRowCellValue;
+            
+        }
+        output.push(newSelectionObject);
+    }
+
+    outputMessageLocation.value = JSON.stringify(output);
 }
