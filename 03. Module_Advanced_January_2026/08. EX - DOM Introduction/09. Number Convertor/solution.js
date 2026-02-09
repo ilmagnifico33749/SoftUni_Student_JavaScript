6d93399438ac78a47b5e4e2c8b62d84ca3ed46fc
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-09 23:20:24 +0200

    09/Feb/2026 - 2

--- 03. Module_Advanced_January_2026/08. EX - DOM Introduction/09. Number Convertor/solution.js
+++ 03. Module_Advanced_January_2026/08. EX - DOM Introduction/09. Number Convertor/solution.js
@@ -1,4 +1,25 @@
 function solve() {
+    const selectMenuTo = document.getElementById('selectMenuTo');
+    selectMenuTo.children[0].textContent = "Choose option";
 
-    //TODO...
+
+    // Add options when the user clicks the dropdown
+    selectMenuTo.addEventListener('click', () => {
+        // Prevent adding duplicates
+        if (selectMenuTo.children.length > 1) return;
+
+        const binary = document.createElement('option');
+        binary.value = 'binary';
+        binary.textContent = 'Binary';
+
+        const hex = document.createElement('option');
+        hex.value = 'hexadecimal';
+        hex.textContent = 'Hexadecimal';
+
+        selectMenuTo.appendChild(binary);
+        selectMenuTo.appendChild(hex);
+
+    });
+    document.getElementById('result').value = "N/A"
+
 }
