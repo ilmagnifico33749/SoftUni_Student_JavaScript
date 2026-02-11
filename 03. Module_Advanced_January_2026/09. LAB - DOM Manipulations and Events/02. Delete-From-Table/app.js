b25bf1edcdd411d944b16ecb1d8fe90c12a6a2b9
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-11 20:01:37 +0200

    11/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let rowsTable = document.querySelectorAll('table tbody tr');
+    let emailToDelete = document.getElementsByName("email")[0].value;
+    let deletionStatusField = document.getElementById('result')
+    
+    for (let row of rowsTable) {
+        console.log(row)
+        let emailAddressCurrentRow = row.querySelectorAll('td')[1].textContent;
+        console.log(emailAddressCurrentRow);
+        if (emailAddressCurrentRow == emailToDelete) {
+            row.parentNode.removeChild(row);
+            deletionStatusField.textContent = "Deleted.";
+            return;
+        }
+    deletionStatusField.textContent = "Not found.";
+    }
 }
