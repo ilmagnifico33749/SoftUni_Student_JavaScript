6686c578809082a04fd5357efe7b88c4683ae492
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-11 23:56:26 +0200

    11/Feb/2026 - 2

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/03. Add-Delete/app.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/03. Add-Delete/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    //TODO...
+    let newLiItem = document.createElement('li');
+    let listOfItems = document.getElementById('items');
+    let userInputTextField = document.getElementById('newItemText');
+    newLiItem.textContent = userInputTextField.value;
+
+    listOfItems.appendChild(newLiItem)
+    userInputTextField.value = '';
+
 }
