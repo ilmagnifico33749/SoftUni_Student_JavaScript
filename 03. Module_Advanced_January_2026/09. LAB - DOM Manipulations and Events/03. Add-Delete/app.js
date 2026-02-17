4bc187c3d6df7635f5cab109e25d665fd7d6d065
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-17 23:33:17 +0200

    17/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/03. Add-Delete/app.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/03. Add-Delete/app.js
@@ -2,9 +2,30 @@ function addItem() {
     let newLiItem = document.createElement('li');
     let listOfItems = document.getElementById('items');
     let userInputTextField = document.getElementById('newItemText');
+
+    if (userInputTextField.value.length === 0) return;
+
     newLiItem.textContent = userInputTextField.value;
 
+    // Creating the deletion link
+    const deleteLink = document.createElement('a');
+    deleteLink.href = '#';
+    deleteLink.textContent = '[Delete]';
+
+    // Attaching the deletion behavior
+    deleteLink.addEventListener('click', deleteItemFunction);
+    listOfItems.appendChild(deleteLink);
+
+    function deleteItemFunction() {
+        newLiItem.remove();
+    }
+    
+    
+    newLiItem.appendChild(deleteLink);
+
     listOfItems.appendChild(newLiItem)
+
     userInputTextField.value = '';
+    Event: mouse
 
 }
