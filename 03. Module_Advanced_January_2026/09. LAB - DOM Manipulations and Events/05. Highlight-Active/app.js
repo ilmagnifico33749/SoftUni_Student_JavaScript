1c55b6ff1a489aa755b91348686342716bfbecdd
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-19 23:47:02 +0200

    19/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/05. Highlight-Active/app.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    let inputsSections = Array.from(document.querySelectorAll("input"));
+    
+    inputsSections.forEach(input => {
+        input.addEventListener('focus', onFocus);
+        input.addEventListener('blur', onBlur);
+    });
+
+    function onFocus(e) {
+        e.target.parentElement.classList.add('focused');
+    }
+
+    function onBlur(e) {
+        e.target.parentElement.classList.remove('focused');
+    }
+
 }
