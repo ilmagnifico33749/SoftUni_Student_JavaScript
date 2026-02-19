0e6f7bc06f004ae5730462682bbe970f33d63f8d
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-20 00:28:54 +0200

    20/Feb/2026 - 2

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/07. Shopping-Cart/solution.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,36 @@
 function solve() {
-   //TODO...
+    const buttons = Array.from(document.querySelectorAll('.add-product'));
+    const checkoutBtn = document.querySelector('.checkout');
+    const output = document.querySelector('textarea');
+
+    let products = new Set();
+    let totalPrice = 0;
+
+    // Add product buttons
+    buttons.forEach(btn => {
+        btn.addEventListener('click', addProduct);
+    });
+
+    // Checkout button
+    checkoutBtn.addEventListener('click', checkout);
+
+    function addProduct(e) {
+        const productElement = e.target.parentElement.parentElement;
+
+        const name = productElement.querySelector('.product-title').textContent;
+        const price = Number(productElement.querySelector('.product-line-price').textContent);
+
+        products.add(name);
+        totalPrice += price;
+
+        output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+    }
+
+    function checkout() {
+        output.value += `You bought ${Array.from(products).join(', ')} for ${totalPrice.toFixed(2)}.`;
+
+        // Disable all buttons
+        buttons.forEach(btn => btn.disabled = true);
+        checkoutBtn.disabled = true;
+    }
 }
