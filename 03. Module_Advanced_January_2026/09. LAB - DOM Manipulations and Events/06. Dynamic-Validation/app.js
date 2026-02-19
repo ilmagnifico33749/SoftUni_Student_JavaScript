a992d6b63eba7795251333524945b854b5412e70
Kiril Pavlov <96083262+ilmagnifico33749@users.noreply.github.com>
2026-02-20 00:09:20 +0200

    20/Feb/2026 - 1

--- 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/06. Dynamic-Validation/app.js
+++ 03. Module_Advanced_January_2026/09. LAB - DOM Manipulations and Events/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.getElementById('email');
+
+    input.addEventListener('change', () => {
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if (pattern.test(input.value)) {
+            input.classList.remove('error');
+        } else {
+            input.classList.add('error');
+        }
+    });
 }
